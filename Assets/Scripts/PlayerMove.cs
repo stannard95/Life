f1879168c69fabe66b7df44f7c041166681257bd
keir stannard <keir stannard>
2017-12-01 16:41:14 +0000

    added player and plane and new asset pack for shading

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CnControls;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float walkSpeed = 3.0F;
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveDirection = new Vector3(CnInputManager.GetAxis("Horizontal"), 0, CnInputManager.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= walkSpeed;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
